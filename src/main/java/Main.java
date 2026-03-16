38ac4ee3d3c47c0b3d58b33d789d70fcc6a36c96
nahomhg <nahom.gh@outlook.com>
2026-03-16 16:36:37 +0000

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -46,44 +46,43 @@ public class Main {
       String response_200 = "HTTP/1.1 200 OK\r\n\r\n";
       String response_404 = "HTTP/1.1 404 Not Found\r\n\r\n";
 
-
       if(readByteCount != -1) {
           String request = new String(buffer, 0, readByteCount, StandardCharsets.UTF_8);
           CustomHttpRequest customHttpRequest = getHttpRequest(request);
-          String output = "";
+          String response = "";
           String fileContent = "";
-
           int indexOfDirectory = 0;
+          
           if(argumentsPassed.length > 0) {
               indexOfDirectory = Arrays.asList(argumentsPassed).indexOf("--directory");
               String fileName = customHttpRequest.path().split("/")[2];
-              if(indexOfDirectory != -1 && !doesFileExist(argumentsPassed[indexOfDirectory+1],fileName)){
+              if(indexOfDirectory != -1 && doesFileExist(argumentsPassed[indexOfDirectory+1],fileName)){
                   System.out.println("got here:\n"+indexOfDirectory+"\t"+doesFileExist(argumentsPassed[indexOfDirectory+1],fileName));
-                  output = response_404;
-              }else{
                   fileContent = getFileContent(argumentsPassed[indexOfDirectory+1]+fileName);
-                  output = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\\nContent-Length: "+fileContent.length()+"\r\n\r\n"+fileContent+"!\n";
+                  response = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\\nContent-Length: "+fileContent.length()+"\r\n\r\n"+fileContent+"!\n";
+              }else{
+                  response = response_404;
               }
           }
-
-          if(customHttpRequest.path().equals("/")){
-              output = response_200;
+          else if(customHttpRequest.path().equals("/")){
+              response = response_200;
           }else if(customHttpRequest.path().startsWith("/echo/") && !customHttpRequest.path().substring(6).isEmpty()) {
               String pathStr = customHttpRequest.path().substring(6);
-              output = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+pathStr.length()+"\r\n\r\n"+pathStr+"\n";
+              response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+pathStr.length()+"\r\n\r\n"+pathStr+"\n";
           }
           else if(customHttpRequest.path().equals("/user-agent")){
               String userAgentContent = getHeader(customHttpRequest.headers(),"User-Agent");
-              output = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+userAgentContent.length()+"\r\n\r\n"+userAgentContent+"\n";
+              response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+userAgentContent.length()+"\r\n\r\n"+userAgentContent+"\n";
           }
           else {
-              output = response_404;
+              response = response_404;
           }
 
-          socket.getOutputStream().write(output.getBytes());
+          socket.getOutputStream().write(response.getBytes());
 
       }
   }
+
   private static CustomHttpRequest getHttpRequest(String request){
       String[] requestArray = request.split("\r\n");
       String requestMethod = requestArray[0].split("\\s+")[0];
@@ -97,6 +96,9 @@ public class Main {
       String body = (i + 1 < requestArray.length) ? requestArray[i+1] : "";
       return new CustomHttpRequest(requestMethod, requestPath, headers, body);
   }
+
+
+  
 
   private static String getHeader(Map<String, String> headers, String headerName){
       return headers.getOrDefault(headerName, "");
