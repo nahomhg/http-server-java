dc8bbb55f190fd1bb5e6e4b77f0b0c3df723d094
nahomhg <nahom.gh@outlook.com>
2026-03-16 18:40:22 +0000

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -57,16 +57,11 @@ public class Main {
               if (indexOfDirectory != -1 && doesFileExist(argumentsPassed[indexOfDirectory + 1], fileName)) {
                   fileContent = getFileContent(argumentsPassed[indexOfDirectory + 1] + fileName);
                   String header = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + fileContent.length + "\r\n\r\n";
+                  fileContent = getFileContent(argumentsPassed[indexOfDirectory + 1] + fileName);
                   OutputStream out = socket.getOutputStream();
                   out.write(header.getBytes(StandardCharsets.UTF_8));
                   out.write(fileContent);
-                /*
-                fileContent = getFileContent(argumentsPassed[indexOfDirectory + 1] + fileName);
-                byte[] customResponse = new CustomHttpResponse().setHttpType("HTTP/1.1").setStatusCode("200 OK").setHeaders(new HashMap<>("Content-Type" : "application/octet-stream", "Content-Length":fileContent.length));
-                OutputStream out = socket.getOutputStream();
-                  out.write(customResponse);
-                  out.write(fileContent);
-                                   */
+
               } else {
                   response = response_404;
               }
@@ -118,107 +113,8 @@ public class Main {
       return Files.readAllBytes(Path.of(filePath));
   }
 
-//  private static void manageFiles(CustomHttpRequest customHttpRequest, String[] argumentsPassed, Socket socket){
-//      try {
-//          byte[] fileContent = "";
-//          String header = "";
-//
-//          int indexOfDirectory = Arrays.asList(argumentsPassed).indexOf("--directory");
-//          String fileName = customHttpRequest.path().split("/")[2];
-//          if (indexOfDirectory != -1 && doesFileExist(argumentsPassed[indexOfDirectory + 1], fileName)) {
-//              System.out.println("got here:\n" + indexOfDirectory + "\t" + doesFileExist(argumentsPassed[indexOfDirectory + 1], fileName));
-//              fileContent = getFileContent(argumentsPassed[indexOfDirectory + 1] + fileName);
-//              System.out.println("file: " + Arrays.toString(fileContent) + "\tlength: " + fileContent.length);
-//              header = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + fileContent.length + "\r\n\r\n";
-//              OutputStream out = socket.getOutputStream();
-//              out.write(header.getBytes(StandardCharsets.UTF_8));
-//              out.write(fileContent);
-//          } else {
-//              response = response_404;
-//          }
-//      }catch(IOException e){
-//          e.printStackTrace();
-//      }
 }
 
 
 
 record CustomHttpRequest(String method, String path, Map<String, String> headers, String body){};
-
-class CustomHttpResponse{
-
-    private String httpType;
-    private String statusCode;
-    private Map<String, String> headers;
-    private byte[] body;
-
-    public CustomHttpResponse(CustomHttpResponseBuilder builder){
-        this.httpType = builder.httpVersion;
-        this.statusCode = builder.statusCode;
-        this.headers = builder.headers;
-        this.body = builder.body;
-    }
-
-    public String getHttpType() {
-        return httpType;
-    }
-
-    public String getStatusCode() {
-        return statusCode;
-    }
-
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
-
-    public byte[] getBody() {
-        return body;
-    }
-
-    public static class CustomHttpResponseBuilder{
-        private String httpVersion;
-        private String statusCode;
-        private Map<String, String> headers;
-        private byte[] body;
-
-        public CustomHttpResponseBuilder httpVersion(String httpType){
-            this.httpVersion = httpType;
-            return this;
-        }
-
-        public CustomHttpResponseBuilder statusCode(String statusCode){
-            this.statusCode = statusCode;
-            return this;
-        }
-
-        public CustomHttpResponseBuilder headers(Map<String, String> headers){
-            this.headers = headers;
-            return this;
-        }
-
-        public CustomHttpResponseBuilder body(byte[] body){
-            this.body = body;
-            return this;
-        }
-
-        public CustomHttpResponse build(){
-            return new CustomHttpResponse(this);
-        }
-
-        public void send(OutputStream outputStream) throws IOException {
-            StringBuilder responseText = new StringBuilder();
-            responseText.append(httpVersion).append(" ").append(statusCode).append("\r\n");
-            for(String i : headers.keySet()){
-                responseText.append(i).append(": ").append(headers.get(i));
-            }
-            responseText.append("\r\n\r\n");
-            outputStream.write(responseText.toString().getBytes(StandardCharsets.UTF_8));
-            if(body!= null){
-                outputStream.write(body);
-            }
-        }
-    }
-/*
-
- */
-}
