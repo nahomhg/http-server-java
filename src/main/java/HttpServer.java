2262971a6a1cd479c56fffb229657c68c72bb821
nahomhg <nahom.gh@outlook.com>
2026-03-24 22:50:27 +0000

    codecrafters submit [skip ci]

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -8,7 +8,6 @@ import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
