2f7504c8e642ac6fae1eae163db62c9df0fc4d11
nahomhg <nahom.gh@outlook.com>
2026-05-03 15:30:28 +0100

    codecrafters submit [skip ci]

--- src/main/java/EchoEncodingHandler.java
+++ src/main/java/EchoEncodingHandler.java
@@ -15,25 +15,29 @@ public class EchoEncodingHandler implements RouteHandler{
 
     @Override
     public HttpResponse handle(CustomHttpRequest request) {
+
         String encoding = request.headers().get("Accept-Encoding");
-        String endpoint = request.path().substring(6);
-        try(ByteArrayOutputStream output = new ByteArrayOutputStream()) {
-            GZIPOutputStream gzip = new GZIPOutputStream(output);
-            byte[] buffer = endpoint.getBytes();
-            gzip.write(buffer,0,buffer.length);
-            gzip.close();
-            return new HttpResponse.HttpResponseBuilder()
-                .setHttpStatus(HttpStatus.OK)
-                .addHeader("Content-Encoding",encoding)
-                .addHeader("Content-Type","text")
-                .addHeader("Content-Length",String.valueOf(buffer.length))
-                    .addBody(buffer)
-                    .build();
-        }catch (IOException exception) {
-            System.err.println("EXCEPTION: "+exception.getMessage());
+        byte[] buffer = request.path().substring(6).getBytes();
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (GZIPOutputStream gzip = new GZIPOutputStream(byteArrayOutputStream)) {
+            gzip.write(buffer);
+        } catch (IOException exception) {
+            System.err.println("EXCEPTION: " + exception.getMessage());
         }
+
+        byte[] compress = byteArrayOutputStream.toByteArray();
+
         return new HttpResponse.HttpResponseBuilder()
-                .setHttpStatus(HttpStatus.NOT_FOUND)
+                .setHttpStatus(HttpStatus.OK)
+                .addHeader("Content-Encoding", encoding)
+                .addHeader("Content-Type", "text")
+                .addHeader("Content-Length", String.valueOf(buffer.length))
+                .addBody(compress)
                 .build();
+
+//        return new HttpResponse.HttpResponseBuilder()
+//                .setHttpStatus(HttpStatus.NOT_FOUND)
+//                .build();
     }
 }
