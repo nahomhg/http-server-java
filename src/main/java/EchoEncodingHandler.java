96f35a9bed8a8ff669550fddf531365f3cd0cae5
nahomhg <nahom.gh@outlook.com>
2026-05-03 14:48:11 +0100

    codecrafters submit [skip ci]

--- src/main/java/EchoEncodingHandler.java
+++ src/main/java/EchoEncodingHandler.java
@@ -3,7 +3,7 @@ public class EchoEncodingHandler implements RouteHandler{
 
     @Override
     public boolean matchesHandler(String endpoint) {
-        System.out.println(this.getClass().getSimpleName()+"endopint "+endpoint);
+        System.out.println(this.getClass().getSimpleName()+"endopint "+endpoint+"\n"+endpoint.substring(6));
         return endpoint.startsWith("/echo/") && endpoint.substring(6).matches("[a-zA-Z]");
     }
 
