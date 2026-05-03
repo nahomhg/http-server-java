2b6e0603122d89d712d52a445aff46210c360392
nahomhg <nahom.gh@outlook.com>
2026-05-03 14:38:40 +0100

    codecrafters submit [skip ci]

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -31,9 +31,10 @@ public class HttpServer {
         this.service = Executors.newFixedThreadPool(10);
         this.serverSocket.setReuseAddress(true);
         this.routerRequest = new Router();
-        this.routerRequest.registerHandler(new EchoHandler());
         this.routerRequest.registerHandler(new EchoEncodingHandler());
         this.routerRequest.registerHandler(new FileHandler(this.directory));
+        this.routerRequest.registerHandler(new EchoHandler());
+
     }
 
     public void startServer() {
