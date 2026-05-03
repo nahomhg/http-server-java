659156a0ef9262f1663d6ca44cd74212cbe42ed6
nahomhg <nahom.gh@outlook.com>
2026-05-03 15:19:06 +0100

    codecrafters submit [skip ci]

--- src/main/java/EchoEncodingHandler.java
+++ src/main/java/EchoEncodingHandler.java
@@ -1,3 +1,7 @@
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -11,10 +15,22 @@ public class EchoEncodingHandler implements RouteHandler{
 
     @Override
     public HttpResponse handle(CustomHttpRequest request) {
-//        String encoding = request.headers().get("Accept-Encoding");
-//        //GZIPOutputStream
-//        GZIPInputStream
-//        return new HttpResponse.HttpResponseBuilder()
+        String encoding = request.headers().get("Accept-Encoding");
+        String endpoint = request.path().substring(6);
+        try(ByteArrayOutputStream output = new ByteArrayOutputStream()) {
+            GZIPOutputStream gzip = new GZIPOutputStream(output);
+            byte[] buffer = endpoint.getBytes();
+            gzip.write(buffer,0,buffer.length)
+            return new HttpResponse.HttpResponseBuilder()
+                .setHttpStatus(HttpStatus.OK)
+                .addHeader("Content-Encoding",encoding)
+                .addHeader("Content-Type","text")
+                .addHeader("Content-Length",String.valueOf(buffer.length))
+                    .addBody(buffer)
+                    .build();
+        }catch (IOException exception) {
+            System.err.println("EXCEPTION: "+exception.getMessage());
+        }//        return new HttpResponse.HttpResponseBuilder()
 //                .setHttpStatus(HttpStatus.OK)
 //                .addHeader("Content-Encoding",encoding)
 //                .addHeader("Content-Type","text")
